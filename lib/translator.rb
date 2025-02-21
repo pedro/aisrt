7dd351536dbc03eb28d3822e7e09df11e8d01048
Pedro Belo <pedrobelo@gmail.com>
2025-02-20 22:25:09 -0800

    some polish

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,6 +1,8 @@
 require "json"
 require "srt"
 
+TEXT = /[A-Za-z]/
+
 class Translator
   def initialize(client, contents, options)
     @client = client
@@ -10,22 +12,49 @@ class Translator
 
   def run
     srt = SRT::File.parse(@contents)
-    
-    unique_texts = srt.lines.map{ |l| id(l) }.select { |t| t.match?(/[A-Za-z]/) }
-    translation_map = translate(unique_texts)
-    
-    srt.lines.each_with_index do |l, i|
-      x = id(l)
-      if translated = translation_map[x]
-        if translated.size > 42
-          parts = translated.split(" ")
-          mid = parts.size / 2
-          translated = parts[0..mid].join(" ") + "\n" + parts[mid+1..-1].join(" ")
+
+    groups = srt.lines.slice_when do |prev, curr|
+      str1 = prev.text.join(" ")
+      str2 = curr.text.join(" ")
+      curr.start_time != prev.end_time || !str1.match?(TEXT) || !str2.match?(TEXT) || str2.start_with?("[")
+    end.to_a
+
+    sentences = groups.map { |g| g.map { |line| id(line) }.join(" ") }.select { |t| t.match?(TEXT) }
+    translation_map = translate(sentences)
+
+    idx = 0
+    new_lines = groups.map do |group|
+      sentence = group.map { |line| id(line) }.join(" ")
+      if tr = translation_map[sentence]
+        res = tr.split.reduce([]) do |lines, word|
+          if lines.empty? || lines.last.length + word.length + 1 > (@options[:max_len] || 42)
+            lines << word
+          else
+            lines[-1] += " " + word
+          end
+          lines
         end
-        l.text = translated.split("\n")
+
+        at = group.first.start_time
+        total_time = group.last.end_time - at
+        time_per_word = total_time / tr.split.size
+
+        res.each_slice(2).map do |lines|
+          dur = lines.join(" ").split.size * time_per_word
+          at += dur
+          SRT::Line.new(
+            sequence: idx += 1,
+            start_time: at - dur,
+            end_time: at,
+            text: lines,
+          )
+        end
+      else
+        group.map { |l| l.sequence = idx += 1; l }
       end
     end
 
+    srt.lines = new_lines.flatten
     srt
   end
 
@@ -33,7 +62,6 @@ class Translator
     translations = {}
   
     batches = texts.each_slice(batch_size)
-    puts "batches: #{batches.size}"
   
     batches.each do |batch|
       prompt = <<~PROMPT
@@ -56,7 +84,6 @@ class Translator
         }
       )
   
-      puts "got: #{response.inspect}"
       raw = response.dig("choices", 0, "message", "content").sub(/\A```(?:json)?\s*/, "").sub(/\s*```\z/, "")
       result = JSON.parse(raw)
 
--- test/test_translator.rb
+++ test/test_translator.rb
@@ -4,18 +4,13 @@ require "minitest/test_task"
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "translator"
 
+system "clear" # really this should be in guard but having trouble with it
+
 describe Translator do
   it "translates" do
-    cli = Minitest::Mock.new
-
-    params = {
-      "choices" => [ { "message" => { "content" => '["TRANSLATED1", "TRANSLATED2"]' }}]
-    }
-
-    cli.expect(:chat, params) do |args|
-      assert_equal "gpt-4o-mini", args[:parameters][:model]
-      assert_equal 2, args[:parameters][:messages].size
-      true
+    cli = setup_cli(["TRANSLATED1", "TRANSLATED2"]) do |model, js|
+      assert_equal "gpt-4o-mini", model
+      assert_equal ["TEST1", "TEST2"], js
     end
     
     srt = <<~SRT
@@ -41,4 +36,117 @@ describe Translator do
     SRT
     assert_equal want, res.to_s
   end
+
+  it "deals with shit responses" do
+    responses = [
+      %Q(\n["TRANSLATED1"]),
+      %Q(```\n["TRANSLATED1"]\n```),
+      %Q(```json\n["TRANSLATED1"]\n```),
+    ]
+
+    responses.each do |variant|
+      cli = setup_cli(variant) do |model, js|
+        assert_equal "gpt-4o-mini", model
+        assert_equal ["TEST1"], js
+      end
+      
+      srt = <<~SRT
+        1
+        00:00:00,000 --> 00:00:05,000
+        TEST1
+      SRT
+
+      t = Translator.new(cli, srt, {})
+      res = t.run
+      want = <<~SRT
+        1
+        00:00:00,000 --> 00:00:05,000
+        TRANSLATED1
+      SRT
+      assert_equal want, res.to_s
+    end
+  end
+
+  it "combines sentences in succession" do
+    cli = setup_cli(["ABC1 DEF1 very long line that will be split somehow1", "END1"]) do |model, js|
+      assert_equal ["ABC DEF very long line that will be split somehow", "END"], js
+    end
+    
+    srt = <<~SRT
+      1
+      00:00:00,000 --> 00:00:05,000
+      ABC
+
+      2
+      00:00:05,000 --> 00:00:10,000
+      DEF
+
+      3
+      00:00:10,000 --> 00:00:15,000
+      very long line that will be split somehow
+
+      3
+      00:00:20,000 --> 00:00:25,000
+      END
+    SRT
+
+    t = Translator.new(cli, srt, { max_len: 5})
+    res = t.run
+    want = <<~SRT
+      1
+      00:00:00,000 --> 00:00:03,000
+      ABC1
+      DEF1
+
+      2
+      00:00:03,000 --> 00:00:06,000
+      very
+      long
+
+      3
+      00:00:06,000 --> 00:00:09,000
+      line
+      that
+
+      4
+      00:00:09,000 --> 00:00:12,000
+      will
+      be
+
+      5
+      00:00:12,000 --> 00:00:15,000
+      split
+      somehow1
+
+      6
+      00:00:20,000 --> 00:00:25,000
+      END1
+    SRT
+    assert_equal want, res.to_s
+  end
+end
+
+def setup_cli(response)
+  cli = Minitest::Mock.new
+
+  if !response.is_a?(String)
+    response = JSON.generate(response)
+  end
+
+  params = {
+    "choices" => [ { "message" => { "content" => response }}]
+  }
+
+  cli.expect(:chat, params) do |args|
+    msgs = args[:parameters][:messages]
+    if msgs.size != 2
+      raise "expected 2 messages, got #{msgs.size}: #{msgs.inspect}"
+    end
+    last = args[:parameters][:messages].last[:content].split("\n").last
+    js = JSON.parse(last)
+    yield(args[:parameters][:model], js)
+    true
+  end
+
+  return cli
 end
