f57ee4391b19ba7769f48ee56ae730bdaf3ca88a
Pedro Belo <pedrobelo@gmail.com>
2025-02-20 20:34:05 -0800

    test something

--- test/test_translator.rb
+++ test/test_translator.rb
@@ -7,7 +7,12 @@ require "translator"
 describe Translator do
   it "translates" do
     cli = Minitest::Mock.new
-    cli.expect(:chat, { "choices" => [ { "message" => { "content" => "{}" }}]}) do |args|
+
+    params = {
+      "choices" => [ { "message" => { "content" => '["TRANSLATED1", "TRANSLATED2"]' }}]
+    }
+
+    cli.expect(:chat, params) do |args|
       assert_equal "gpt-4o-mini", args[:parameters][:model]
       assert_equal 2, args[:parameters][:messages].size
       true
@@ -16,11 +21,24 @@ describe Translator do
     srt = <<~SRT
       1
       00:00:00,000 --> 00:00:05,000
-      TEST
+      TEST1
+
+      2
+      00:00:10,000 --> 00:00:20,000
+      TEST2
     SRT
 
     t = Translator.new(cli, srt, {})
     res = t.run
-    assert_equal "1\n00:00:00,000 --> 00:00:05,000\nTEST\n", res.to_s
+    want = <<~SRT
+      1
+      00:00:00,000 --> 00:00:05,000
+      TRANSLATED1
+
+      2
+      00:00:10,000 --> 00:00:20,000
+      TRANSLATED2
+    SRT
+    assert_equal want, res.to_s
   end
 end
